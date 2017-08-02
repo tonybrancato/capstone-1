903a75176f4f98a66da1cba8c597bce536fe7fb9
Tony Brancato <tonybrancato1@gmail.com>
2017-08-02 00:13:11 -0400

    working title search

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-let settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.edamam.com/search?q=beef&app_id=043a2aae&app_key=5b559e45b1e2e78391957184544bfc95&from=0&to=3&calories=gte%20591%2C%20lte%20722&health=alcohol-free",
-  "method": "GET",
-  "headers": {
-    "cache-control": "no-cache",
-    "postman-token": "5922ae12-1471-537b-45c2-24e7e9ed3d3c"
-  }
+function getDataFromApi(searchTerm) {
+	const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://api.edamam.com/search?from=0&calories=gte%20591%2C%20lte%20722&health=alcohol-free",
+	"method": "GET",
+	"headers": {
+			"cache-control": "no-cache",
+	},
+	data: {
+		q: `${searchTerm}`,
+		app_id: '043a2aae',
+		app_key: '5b559e45b1e2e78391957184544bfc95',
+		to: '2'
+	},
+	dataType: 'json',
+//	success: callback
+	}
+
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
 }
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    console.log('query is ',query);
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query);
+  });
+}
+
+$(watchSubmit);
